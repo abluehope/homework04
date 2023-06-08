094a6cd3e46acbd678f7a062e46a4e8a5314395d
abluehope <100765576+abluehope@users.noreply.github.com>
2023-06-08 17:05:30 +0900

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,22 @@
+$(function () {
+  $(window).on("scroll", function () {
+    let sct = $(window).scrollTop();
+    sct > 0 ? $(".header").addClass("on") : $(".header").removeClass("on");
+  });
+
+  $(".main_slide").slick({
+    pauseOnHover: false,
+    arrows: false,
+    autoplay: true,
+    autoplaySpeed: 5000,
+  });
+
+  $(".to_top").on("click", function () {
+    $("html, body").animate({ scrollTop: 0 }, 600);
+  });
+
+  $(window).on("scroll", function () {
+    let sct = $(window).scrollTop();
+    sct > 50 ? $(".to_top").addClass("on") : $(".to_top").removeClass("on");
+  });
+});
